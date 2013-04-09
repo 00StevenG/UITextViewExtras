250cb29afa75cdf5bb6ac2d2ba5c2a0ddd9b0a17
Steven Grace <steven@works5.com>
2013-04-09 12:29:26 -0300

    Path functions now return UIBezierPath

--- UITextView+Extras.m
+++ UITextView+Extras.m
@@ -7,38 +7,12 @@
 //
 
 #import "UITextView+Extras.h"
-#import <objc/runtime.h>
-
 
 #pragma mark - path creation (Internal)
-//
-// Creates path for a given array of UITextSelectionRects
-//
-CGPathRef _CGPathForSelectionRects(NSArray* rects){
-    
-    
-    if([rects count]==0)
-        return NULL;
-    
-    
-    CGMutablePathRef mPath = CGPathCreateMutable();
 
-    for(UITextSelectionRect* selectionRect in rects){
-            
-        CGRect aRect = selectionRect.rect;
-        CGPathAddRect(mPath,NULL,aRect);
-    }
-    
-    CGPathCloseSubpath(mPath);
-
-    CGPathRef final =  CGPathCreateCopy(mPath);
-    CGPathRelease(mPath);
-    CFBridgingRetain(CFBridgingRelease(final));
-    
-    return final;
-}
 
 @implementation UITextView (Extras)
+
 
 #pragma mark - Class Methods
 //
@@ -47,11 +21,36 @@ CGPathRef _CGPathForSelectionRects(NSArray* rects){
 +(UIColor*)defaultSelectionColor{
     return [UIColor colorWithRed:0.788 green:0.867 blue:0.937 alpha:.75];
 }
+#pragma mark - Path Creation (Internal)
+//
+// Assembles a beizier path by adding each CGRect in selectionRects
+//
+-(UIBezierPath*)_bezierPathWithTextSelectionRects:(NSArray*)selectionRects{
+    
+    
+    if([selectionRects count]==0)
+        return nil;
+
+    CGMutablePathRef mPath = CGPathCreateMutable();
+    
+    for(UITextSelectionRect* selectionRect in selectionRects){
+        
+        CGRect aRect = selectionRect.rect;
+        CGPathAddRect(mPath,NULL,aRect);
+    }
+    
+    CGPathCloseSubpath(mPath);
+    
+    UIBezierPath* bPath = [UIBezierPath bezierPathWithCGPath:mPath];
+    CGPathRelease(mPath);
+    return bPath;
+    
+}
 #pragma mark - Selection Paths
 //
+// find the closest text position-> enclosing range and then calculate the enclosing path
 //
-//
--(CGPathRef)selectionPathWithGranularity:(UITextGranularity)granularity atPoint:(CGPoint)pt{
+-(UIBezierPath*)selectionPathWithGranularity:(UITextGranularity)granularity atPoint:(CGPoint)pt{
     
     UITextPosition* pos = [self closestPositionToPoint:pt];
     id<UITextInputTokenizer> tokenizer =self.tokenizer;
@@ -62,17 +61,22 @@ CGPathRef _CGPathForSelectionRects(NSArray* rects){
     
     
     NSArray* rects = [self selectionRectsForRange:textRange];
-    return _CGPathForSelectionRects(rects);
-    
+    return [self _bezierPathWithTextSelectionRects:rects];
     
 }
 //
+// find the position given an index from the beginngin of the doc and then calculate enclosing range
 //
-//
--(CGPathRef)selectionPathWithGranularity:(UITextGranularity)granularity atIndex:(NSUInteger)idx{
+-(UIBezierPath*)selectionPathWithGranularity:(UITextGranularity)granularity atIndex:(NSUInteger)idx{
   
     UITextPosition* pos = [self positionFromPosition:self.beginningOfDocument
                                               offset:idx];
+    
+    
+    // the passed index is <0 or greater than the length
+    if(!pos)
+        return nil;
+    
     
     id<UITextInputTokenizer> tokenizer =self.tokenizer;
     UITextRange* textRange =
@@ -81,20 +85,29 @@ CGPathRef _CGPathForSelectionRects(NSArray* rects){
                           inDirection:UITextWritingDirectionLeftToRight];
     
     NSArray* rects = [self selectionRectsForRange:textRange];
-    return _CGPathForSelectionRects(rects);
+    return [self _bezierPathWithTextSelectionRects:rects];
     
 }
 #pragma mark - Path for Range
 //
+// Grab the selection rects and assemble a paht
 //
-//
--(CGPathRef)pathForRange:(NSRange)range{
+-(UIBezierPath*)pathForRange:(NSRange)range{
     
     NSArray* rects = [self textSelectionRectsForRange:range];
-    return  _CGPathForSelectionRects(rects);
+    return [self _bezierPathWithTextSelectionRects:rects];
 }
 //
+// Calculate the path and its bounding rect
 //
+-(CGRect)boundingRectForRange:(NSRange)range{
+    
+    UIBezierPath* bPath = [self pathForRange:range];
+    return CGPathGetBoundingBox(bPath.CGPath);
+}
+#pragma mark - Caret Rects
+//
+// Find position from the beginning of Doc calculate the caret rect
 //
 -(CGRect)caretRectForStringIndex:(NSUInteger)idx{
     
@@ -102,11 +115,16 @@ CGPathRef _CGPathForSelectionRects(NSArray* rects){
                                               inDirection:UITextStorageDirectionForward
                                                    offset:idx];
     
+    // the passed index is <0 or greater than the length
+    if(!startPos)
+        return CGRectNull;
+    
+    
     return [self caretRectForPosition:startPos];
     
 }
 //
-//
+// Find the position at point and returns its care rect
 //
 -(CGRect)caretRectClosestToPoint:(CGPoint)pt{
     
@@ -116,13 +134,15 @@ CGPathRef _CGPathForSelectionRects(NSArray* rects){
 }
 #pragma mark - Substrings
 //
-//
+//  Find the position at the point and the range for that position it's substring range
 //
 -(NSString*)substringWithGranularity:(UITextGranularity)granularity atPoint:(CGPoint)pt{
     
     UITextPosition* pos = [self closestPositionToPoint:pt];
     id<UITextInputTokenizer> tokenizer =self.tokenizer;
     
+    
+    // may return nil if the given granularity is not found at this position
     UITextRange* textRange =
     [tokenizer rangeEnclosingPosition:pos
                       withGranularity:granularity
@@ -135,19 +155,20 @@ CGPathRef _CGPathForSelectionRects(NSArray* rects){
     
 }
 //
+// Find position and it's offset from the beginning of the document
 //
-//
--(NSUInteger)closestCharacterPositionToPoint:(CGPoint)pt{
+-(NSUInteger)closestCharacterIndexToPoint:(CGPoint)pt{
     
     UITextPosition* pos = [self closestPositionToPoint:pt];
     NSUInteger idx = [self offsetFromPosition:self.beginningOfDocument
                                    toPosition:pos];
     
+    
     return idx;
 }
 #pragma mark - Hit Testing
 //
-//
+// Iterate through the selection rect for the passed range and text against rect containment
 //
 -(BOOL)pointInside:(CGPoint)point inRange:(NSRange)range{
  
@@ -161,10 +182,11 @@ CGPathRef _CGPathForSelectionRects(NSArray* rects){
 }
 #pragma mark - NSRange and UITextRange Conversion
 //
-//
+// convert range to positions -> text Range
 //
 -(UITextRange*)textRangeForRange:(NSRange)range{
     
+    // TODO: test add checks for valid range within in document
     
     UITextPosition* startPos = [self positionFromPosition:self.beginningOfDocument
                                               inDirection:UITextStorageDirectionForward
@@ -178,7 +200,7 @@ CGPathRef _CGPathForSelectionRects(NSArray* rects){
     return textRange;
 }
 //
-//
+// Calculate the location and length -> create NSRange
 //
 -(NSRange)rangeForTextRange:(UITextRange*)textRange{
     
@@ -189,23 +211,20 @@ CGPathRef _CGPathForSelectionRects(NSArray* rects){
     return NSMakeRange(location,length);
 }
 //
-//
+// Convert the textRange (above) and calculate the selection rects
 //
 -(NSArray*)textSelectionRectsForRange:(NSRange)range{
     
-
     UITextRange* textRange = [self textRangeForRange:range];
-    
     NSArray* rects = [self selectionRectsForRange:textRange];
-    
     return rects;
-    
 }
 //
-//
+// Calculate the positions using the origin of the passed rect
+// and 'lower right corner of the rect -> create a NSRange from the result
 //
 -(NSRange)rangeOfSubstringRangeBoundByRect:(CGRect)rect{
-    
+  
     UITextPosition* start = [self closestPositionToPoint:rect.origin];
     UITextPosition* end =
     [self closestPositionToPoint:CGPointMake(CGRectGetMaxX(rect),CGRectGetMaxY(rect))];
@@ -216,25 +235,6 @@ CGPathRef _CGPathForSelectionRects(NSArray* rects){
 }
 #pragma mark - Granularity
 //
-//
-//
--(NSRange)rangeWithGranularity:(UITextGranularity)granularity atPoint:(CGPoint)pt{
-    
-    UITextPosition* pos = [self closestPositionToPoint:pt];
-    id<UITextInputTokenizer> tokenizer =self.tokenizer;
-    UITextRange* textRange =
-    [tokenizer rangeEnclosingPosition:pos
-                      withGranularity:granularity
-                          inDirection:UITextWritingDirectionLeftToRight];
-    
-    if(textRange){
-        return [self rangeForTextRange:textRange];
-    }
-    else{
-        return NSMakeRange(0,0);
-    }
-}
-//
 // Private method to calculate the next UITextPosition from a given positiom
 // and direction
 //
@@ -243,7 +243,7 @@ CGPathRef _CGPathForSelectionRects(NSArray* rects){
                                    inDirection:(UITextStorageDirection)direction{
     
     
-
+    
     UITextPosition* nextPos = [self.tokenizer positionFromPosition:pos
                                                         toBoundary:granularity
                                                        inDirection:direction];
@@ -271,15 +271,39 @@ CGPathRef _CGPathForSelectionRects(NSArray* rects){
     
 }
 //
+// Calculate the position and enclosing textRange -> conver the UITextRange to NSRange
 //
+-(NSRange)rangeWithGranularity:(UITextGranularity)granularity atPoint:(CGPoint)pt{
+    
+    UITextPosition* pos = [self closestPositionToPoint:pt];
+    id<UITextInputTokenizer> tokenizer =self.tokenizer;
+    UITextRange* textRange =
+    [tokenizer rangeEnclosingPosition:pos
+                      withGranularity:granularity
+                          inDirection:UITextWritingDirectionLeftToRight];
+    
+    if(textRange){
+        return [self rangeForTextRange:textRange];
+    }
+    else{
+        return NSMakeRange(0,0);
+    }
+}
+//
+// Calculate the postion at the passed Index, positions and their textRange
+// convert to an NSRange
 //
 -(NSRange)nextRangeWithGranularity:(UITextGranularity)granularity
                          fromIndex:(NSUInteger)idx
                        inDirection:(UITextStorageDirection)direction{
     
-    
     UITextPosition* pos = [self positionFromPosition:self.beginningOfDocument
                                               offset:idx];
+    
+    // the passed index is <0 or greater than the length
+    if(!pos)
+        return NSMakeRange(0,0);
+    
     
     UITextPosition* fromPos = [self _nextPositionWithGranularity:granularity
                                                     fromPosition:pos
@@ -292,6 +316,7 @@ CGPathRef _CGPathForSelectionRects(NSArray* rects){
     UITextRange* textRange = [self textRangeFromPosition:fromPos toPosition:toPos];
     
     return [self rangeForTextRange:textRange];
+    
 }
 
 
